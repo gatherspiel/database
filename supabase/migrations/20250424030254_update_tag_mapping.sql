82e0a34962ab94fb5955f5a48369998c911b1800
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-04-23 23:09:59 -0400

    Update tag mapping

---
+++ supabase/migrations/20250424030254_update_tag_mapping.sql
@@ -0,0 +1,12 @@
+DROP TABLE event_tag_map CASCADE;
+
+CREATE TABLE if not exists location_tag (
+     id SERIAL PRIMARY KEY,
+     name varchar NOT NULL,
+);
+
+
+CREATE TABLE if not exists location_tag_mapping (
+    location_tag_id varchar REFERENCES location_tag,
+    location_id varchar REFERENCES locations
+);
