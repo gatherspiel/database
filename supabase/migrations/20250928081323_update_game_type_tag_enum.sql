47a8dc3f0b9a6f005ca5b475df4a857d508bf37c
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-09-28 04:15:10 -0400

    Update game tags enum

---
+++ supabase/migrations/20250928081323_update_game_type_tag_enum.sql
@@ -0,0 +1,3 @@
+ALTER TYPE game_type_tag RENAME VALUE 'Light Games' TO 'Light games'
+ALTER TYPE game_type_tag RENAME VALUE 'Social Games' TO 'Social games'
+ALTER TYPE game_type_tag RENAME VALUE 'Hidden Identity Games' TO 'Hidden identity games'
