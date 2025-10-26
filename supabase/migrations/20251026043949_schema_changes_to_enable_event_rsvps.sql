e01debbc17949de8b35f10c1c9b0d2907dc5630a
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-10-26 01:38:14 -0400

    Updates

--- supabase/migrations/20251026043949_schema_changes_to_enable_event_rsvps.sql
+++ supabase/migrations/20251026043949_schema_changes_to_enable_event_rsvps.sql
@@ -2,7 +2,7 @@ create table public.event_rsvp (
   user_id integer null,
   event_id integer null,
   rsvp_time timestamp with time zone null,
-  constraint event_rsvp_pkey (user_id, event_id),
+  constraint event_rsvp_pkey primary KEY (user_id, event_id),
   constraint event_rsvp_event_id_fkey foreign KEY (event_id) references events (id),
   constraint event_rsvp_user_id_fkey foreign KEY (user_id) references users (id),
 )
