2999babd5a7273c7a3d375ded045ab084c7cdcb8
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-11-03 21:29:40 -0500

    Update

---
+++ supabase/migrations/20251104022808_combine_event_rsvp_and_event_admin_tables.sql
@@ -0,0 +1,9 @@
+DROP table if exists event_rsvp;
+
+DO $$ BEGIN
+   ALTER TYPE event_admin_level ADD VALUE  'event_moderator';
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
+ALTER table event_admin_data ADD column rsvp_time timestamp;
