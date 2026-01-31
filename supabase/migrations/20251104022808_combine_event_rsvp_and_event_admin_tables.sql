e94dc2edd79922b5db8a896b98e511d357fe6e26
Bharat Ponnaluri <gulu@createthirdplaces.com>
2026-01-30 21:07:13 -0500

    Fix migration

--- supabase/migrations/20251104022808_combine_event_rsvp_and_event_admin_tables.sql
+++ supabase/migrations/20251104022808_combine_event_rsvp_and_event_admin_tables.sql
@@ -6,6 +6,6 @@ EXCEPTION
     WHEN duplicate_object THEN null;
 END $$;
 
-ALTER table event_admin_data ADD column rsvp_time timestamp;
+ALTER table event_admin_data ADD column IF NOT EXISTS rsvp_time timestamp;
 
 ALTER table event_admin_data ADD constraint event_admin_data_event_id_fkey foreign KEY (event_id) references events (id)
